a4e347e3c7003c77c6ea11c1042264505de3da89
Jeremy Johnston <jpj054000@gmail.com>
2014-12-05 21:31:23 -0600

    Handling unicode and output of clean doc
    Still more work to do but readjson.py now writes a clean doc, while
    recognizing utf-8 encoded article body. Result is clean output with
    characters replacing unicode codes.
    
    TODO: Sentence boundary detection

--- readjson.py
+++ readjson.py
@@ -2,14 +2,50 @@ import json
 import sys
 import getopt
 import os
-
+import codecs
 
 def read(fileName):
-    file = open(fileName)
-    data = json.load(file)
-    
-    return data
+    try:
+        file = codecs.open(fileName, encoding='utf-8')
+        data = json.load(file)
+        file.close()
+        return data
+    except:
+        print "ERROR reading {0}".format(fileName)
+        raise
 
+def writeDoc(item, fileName):
+    try:
+        print "Writing doc {0}...".format(fileName)
+        file = open(fileName, "w")
+        link = item['link']
+        date = item['date'][0]
+        title = item['title'][0]
+        
+        body = item['body']
+        body = cleanBody(body)
+        
+        file.write("{0}\n".format(link))
+        file.write("{0}\n".format(date))
+        file.write("{0}\n".format(title))
+        file.write("{0}\n".format(body.encode('utf-8')))
+        
+        file.close()
+    except:
+        print "ERROR writing {0}".format(fileName)
+        raise
+        
+def cleanBody(body):
+    """
+    Clean any other markup, and join paragraphs.
+    """
+    text = u""
+    for p in body:
+        text = text + p + '\n'
+        
+    return text
+        
+        
 def printItem(item):
     print "Link: ", item['link']
     print "Date: ", item['date']
@@ -19,14 +55,17 @@ def printItem(item):
     
 def main(argv):
     jsonFileName = ""
+    outputFileName = ""
 
     try:
-        opts, args = getopt.getopt(argv, "f:",["file="])
+        opts, args = getopt.getopt(argv, "i:o:",["input=,output="])
     except getopt.GetoptError:
         sys.exit(2)
     for opt, arg in opts:  
-        if opt in ("-f", "--file"):
+        if opt in ("-i", "--input"):
             jsonFileName = arg
+        elif opt in ("-o", "--output"):
+            outputFileName = arg
             
     if not os.path.exists(jsonFileName):
         print '\nPath {0} does not exist'.format(jsonFileName)
@@ -36,7 +75,10 @@ def main(argv):
     
     print "Items: ", len(data)
     print "First item\n---\n "
-    printItem(data[0])
+    printItem(data[0]) 
+    
+    writeDoc(data[0], outputFileName)
+    print "DONE writing docs"
     
   
 if __name__ == "__main__":
